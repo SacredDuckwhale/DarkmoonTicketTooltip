12191b3be22a9a3f4822a0b3d2c749671a37ee0f
Sacred Duckwhale <duckwhale@mail.ru>
2017-10-05 18:59:06 +0200

    Added quest title to tooltip

--- DTT.lua
+++ DTT.lua
@@ -21,18 +21,18 @@ local L = LibStub("AceLocale-3.0"):GetLocale("DarkmoonTicketTooltip", false)
 
 -- Database of turnin items
 local questItems = {
-	[71715] = { ["quest"] = 29451, ["tickets"] = 15 }, -- A Treatise on Strategy
-	[71638] = { ["quest"] = 29446, ["tickets"] = 10 }, -- Ornate Weapon
-	[71637] = { ["quest"] = 29445, ["tickets"] = 10 }, -- Mysterious Grimoire
-	[71636] = { ["quest"] = 29444, ["tickets"] = 10 }, -- Monstrous Egg
-	[71635] = { ["quest"] = 29443, ["tickets"] = 10 }, -- Imbued Crystal
-	[71716] = { ["quest"] = 29464, ["tickets"] = 10 }, -- Soothsayer's Runes
-	[71952] = { ["quest"] = 29457, ["tickets"] = 5 }, -- Captured Insignia
-	[71951] = { ["quest"] = 29456, ["tickets"] = 5 }, -- Banner of the Fallen
-	[71953] = { ["quest"] = 29458, ["tickets"] = 5 }, -- Adventurer's Journal
+	[71715] = { ["quest"] = 29451, ["tickets"] = 15, title = L["The Master Strategist"] }, -- A Treatise on Strategy
+	[71638] = { ["quest"] = 29446, ["tickets"] = 10, title = L["A Wondrous Weapon"] }, -- Ornate Weapon
+	[71637] = { ["quest"] = 29445, ["tickets"] = 10, title = L["An Intriguing Grimoire"] }, -- Mysterious Grimoire
+	[71636] = { ["quest"] = 29444, ["tickets"] = 10, title = L["An Exotic Egg"] }, -- Monstrous Egg
+	[71635] = { ["quest"] = 29443, ["tickets"] = 10, title = L["A Curious Crystal"] }, -- Imbued Crystal
+	[71716] = { ["quest"] = 29464, ["tickets"] = 10, title = L["Tools of Divination"] }, -- Soothsayer's Runes
+	[71952] = { ["quest"] = 29457, ["tickets"] = 5, title = L["The Enemy's Insignia"] }, -- Captured Insignia
+	[71951] = { ["quest"] = 29456, ["tickets"] = 5, title = L["A Captured Banner"] }, -- Banner of the Fallen
+	[71953] = { ["quest"] = 29458, ["tickets"] = 5, title = L["The Captured Journal"] }, -- Fallen Adventurer's Journal
 	
-	[105891] = { quest = 33354, tickets = 10 }, -- Moonfang's Pelt -> Den Mother's Demise (probably too expensive in almost all cases, but added for completion)
-	[124669] = { quest = 38934, tickets = 100, numRequiredItems = 100 }, -- Darkmoon Daggermaw -> Silas' Secret Stash (one-time only)
+	[105891] = { quest = 33354, tickets = 10, title = L["Den Mother's Demise"] }, -- Moonfang's Pelt -> Den Mother's Demise (probably too expensive in almost all cases, but added for completion)
+	[124669] = { quest = 38934, tickets = 100, title = L["Silas' Secret Stash"], numRequiredItems = 100 }, -- Darkmoon Daggermaw -> Silas' Secret Stash (one-time only)
 }
 
 -- Display tooltip information, but only on DMF quest items
@@ -46,10 +46,11 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self)
 			if questItems[itemID] then -- Is DMF turnin item -> Show tooltip info
 			
 				-- Check if quest was already completed 
+				local questTitle = questItems[itemID]["title"]
 				if IsQuestFlaggedCompleted(questItems[itemID]["quest"]) then -- Is completed - > Show text in green
-					self:AddLine(L["Quest already completed!"], 0xFF/255, 0x1A/255, 0x1A/255)
+					self:AddLine(format(L["Quest \"%s\" is already completed!"], questTitle), 0xFF/255, 0x1A/255, 0x1A/255)
 				else -- Not yet completed -> Show text in red
-					self:AddLine(L["Quest not yet completed!"], 0x1E/255, 0xFF/255, 0x00/255)
+					self:AddLine(format(L["Quest \"%s\" is not yet completed!"], questTitle), 0x1E/255, 0xFF/255, 0x00/255)
 				end
 			
 				local numTickets = questItems[itemID]["tickets"]
--- Locales/enGB.lua
+++ Locales/enGB.lua
@@ -1,7 +1,18 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("DarkmoonTicketTooltip", "enGB", true)
 
+L["A Captured Banner"] = true
+L["A Curious Crystal"] = true
+L["A Wondrous Weapon"] = true
+L["An Exotic Egg"] = true
+L["An Intriguing Grimoire"] = true
 L["Darkmoon Prize Tickets"] = true
+L["Den Mother's Demise"] = true
 L["Price per ticket: %s%s%s"] = true
-L["Quest already completed!"] = true
+L["Quest \"%s\" is already completed!"] = true
+L["Quest \"%s\" is not yet completed!"] = true
 L["Quest awards %d %s"] = true
-L["Quest not yet completed!"] = true
+L["Silas' Secret Stash"] = true
+L["The Captured Journal"] = true
+L["The Enemy's Insignia"] = true
+L["The Master Strategist"] = true
+L["Tools of Divination"] = true
