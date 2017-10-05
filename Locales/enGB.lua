48d8fda1f4d37fd3caeca04a06a2eda2ff4c171c
Sacred Duckwhale <duckwhale@mail.ru>
2017-10-05 18:17:26 +0200

    Added missing phrases (via scrapewhale)

--- Locales/enGB.lua
+++ Locales/enGB.lua
@@ -2,4 +2,6 @@ local L = LibStub("AceLocale-3.0"):NewLocale("DarkmoonTicketTooltip", "enGB", tr
 
 L["Darkmoon Prize Tickets"] = true
 L["Price per ticket: %s%s%s"] = true
+L["Quest already completed!"] = true
 L["Quest awards %d %s"] = true
+L["Quest not yet completed!"] = true
